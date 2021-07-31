bf5ecbef0ba7cb83875f742269e9b42ab83616eb
Eric Joseph <ericjkurusinkal@gmail.com>
2021-07-31 22:06:01 +0530

    AppBar, Toolbar, and basic React Player are implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+// import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import ReactPlayer from 'react-player';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+            <Typography variant="h6">React Video Player</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md">
+        <ReactPlayer url="https://www.youtube.com/watch?v=r6qWEteVMyM&list=PL4OKShK9gkQca9QVqmnPMzT6QYM2LHaqt&index=3"
+        muted={false} playing={true}/>
+      </Container>
+    </>
   );
 }
 
